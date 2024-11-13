b96f2f1ae5c7b441d00bc8f1be86b57e8a221466
Noe Desprez <ndesprez@c1r1p1.42nice.fr>
2024-11-13 16:28:11 +0100

    training + predict

--- data_classification/predict.py
+++ data_classification/predict.py
@@ -1,34 +1,97 @@
 import argparse
 import os
 import sys
+import numpy as np
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import img_to_array
+
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import utils as utils
 
 
+MODEL_PATH = 'trained_model.keras'
+
+
+class_indices = {
+    0: 'Apple_Black_rot',
+    1: 'Apple_Healthy',
+    2: 'Apple_Rust',
+    3: 'Apple_Scab',
+    4: 'Grape_Black_rot',
+    5: 'Grape_Esca',
+    6: 'Grape_Healthy',
+    7: 'Grape_Spot'
+}
+
+
+def load_and_predict(img_path):
+    """
+    Loads a trained model and predicts the class of a given image.
+
+    Preprocesses the image by resizing it, converting it to a numpy array,
+    expanding dimensions to simulate a batch, and normalizing pixel values.
+    Then, it uses the loaded model to make a prediction on the image.
+
+    Args:
+        img_path (str): The file path to the image to be classified.
+
+    Returns:
+        None: Prints the predicted class name for the image.
+    """
+
+    model = load_model(MODEL_PATH)
+
+    # Redimensionner l'image à 128x128
+    img = image.load_img(img_path, target_size=(128, 128))
+    # Convertir l'image en array numpy
+    img_array = img_to_array(img)
+    # Ajouter une dimension pour simuler un batch
+    img_array = np.expand_dims(img_array, axis=0)
+    # Normaliser les pixels entre 0 et 1
+    img_array = img_array / 255.0
+
+    prediction = model.predict(img_array)
+
+    predicted_class = np.argmax(prediction, axis=1)
+
+    predicted_class_name = class_indices[predicted_class[0]]
+    print(f"Image class: {predicted_class_name}")
+
+
 def main(image):
     """
-    Main function to extract and analyze the dataset from images
-    and generate charts.
+    Main function to validate the image path and perform the prediction.
 
-    Parameters:
-        image (str): The image path.
-    Returns: None
-    Raises: None
+    Checks that the specified image file exists using a utility function.
+    Then, calls `load_and_predict()` to classify the image.
+
+    Args:
+        image (str): The file path to the image to be classified.
+
+    Returns:
+        None
     """
 
     utils.check_file(image)
+    load_and_predict(image)
+
 
 if __name__ == "__main__":
     try:
         parser = argparse.ArgumentParser(
             prog="Predict",
-            description="Predict the class of an image using a previously trained model"
+            description="Predicts the class of an image "
+            "using a previously trained model"
         )
         parser.add_argument("image", type=str,
                             help="Path to an image to predict")
         args = parser.parse_args()
-        main(args.images_directory)
+        main(args.image)
 
     except Exception as e:
         print(f"predict.py: error: {e}")
--- data_classification/train.py
+++ data_classification/train.py
@@ -1,20 +1,155 @@
 import os
 import sys
-import random
 import shutil
 import argparse
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, \
+    Flatten, Dense, Dropout, Input
+
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import utils as utils
 
-DATASET_PATH = "./../images_dataset"
 
-def promt_reloading(max_retries=2):
-    """Prompts the user to confirm reloading the dataset if it already exists."""
+DATASET_PATH = "./../images_dataset"
+MODEL_NAME = "trained_model.keras"
+
+
+def train():
+    """
+    Trains a convolutional neural network (CNN)
+    on the image dataset with data augmentation.
+
+    Uses `ImageDataGenerator` to preprocess the images and performs training
+    with a specified architecture of convolutional layers, dense layers,
+    and dropout. Saves the trained model to a file specified by MODEL_NAME.
+
+    Returns:
+        None
+    """
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=20,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True
+    )
+
+    validation_datagen = ImageDataGenerator(rescale=1./255)
+
+    train_generator = train_datagen.flow_from_directory(
+        '../images_dataset/train',
+        target_size=(128, 128),
+        batch_size=32,
+        class_mode='categorical'
+    )
+
+    validation_generator = validation_datagen.flow_from_directory(
+        '../images_dataset/val',
+        target_size=(128, 128),
+        batch_size=32,
+        class_mode='categorical'
+    )
+
+    model = Sequential([
+        Input(shape=(128, 128, 3)),
+        Conv2D(32, (3, 3), activation='relu'),
+        MaxPooling2D(2, 2),
+        Conv2D(64, (3, 3), activation='relu'),
+        MaxPooling2D(2, 2),
+        Conv2D(128, (3, 3), activation='relu'),
+        MaxPooling2D(2, 2),
+        Flatten(),
+        Dense(256, activation='relu'),
+        Dropout(0.5),
+        Dense(len(train_generator.class_indices), activation='softmax')
+    ])
+
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.fit(
+        train_generator,
+        epochs=20,
+        validation_data=validation_generator,
+        verbose=1
+    )
+
+    model.save(MODEL_NAME)
+
+
+def promt_reloading_tm(max_retries=2):
+    """
+    Prompts the user to confirm retraining
+    the model if it already exists.
+    """
     attempts = 0
     while attempts < max_retries:
-        user_input = input(f"train.py: The dataset path '{DATASET_PATH}' already exists.\n"
-                           "train.py: Do you want to reload the dataset? (yes/no): ").strip().lower()
+        user_input = input(f"train.py: The trained model '{MODEL_NAME}' "
+                           "already exists.\n"
+                           "train.py: Do you want to retrain the model? "
+                           "(yes/no): ").strip().lower()
+        if user_input in {"yes", "y"}:
+            return True
+        elif user_input in {"no", "n"}:
+            print("train.py: Retraining canceled.")
+            return False
+        else:
+            print("Error: Invalid input. Please enter 'yes' or 'no'.")
+            attempts += 1
+    print("train.py: Too many invalid attempts. Retraining canceled.")
+    return False
+
+
+def train_model():
+    """
+    Manages model training, including confirmation prompts, existing model
+    deletion, and error handling.
+
+    Checks if a model file already exists, prompting the user for confirmation
+    before overwriting. Deletes the existing model if confirmed and calls
+    `train()` to perform the training.
+
+    Returns:
+        None
+    """
+    if os.path.exists(MODEL_NAME) and not promt_reloading_tm():
+        return
+
+    try:
+        if os.path.exists(MODEL_NAME):
+            os.remove(MODEL_NAME)
+            print("train.py: Deleting previous trained model")
+        train()
+        print("train.py: Model training completed. " +
+              f"Trained model saved as '{MODEL_NAME}'")
+
+    except Exception as e:
+        print(f"train.py: Error occurred while training model: {str(e)}")
+
+
+def promt_reloading_ds(max_retries=2):
+    """
+    Prompts the user to confirm reloading
+    the dataset if it already exists.
+    """
+    attempts = 0
+    while attempts < max_retries:
+        user_input = input(f"train.py: The dataset path '{DATASET_PATH}' "
+                           "already exists.\n"
+                           "train.py: Do you want to reload the dataset? "
+                           "(yes/no): ").strip().lower()
         if user_input in {"yes", "y"}:
             return True
         elif user_input in {"no", "n"}:
@@ -29,7 +164,7 @@ def promt_reloading(max_retries=2):
 
 def load_dataset(directory):
     """Main function to load, balance, and split the dataset."""
-    if os.path.exists(DATASET_PATH) and not promt_reloading():
+    if os.path.exists(DATASET_PATH) and not promt_reloading_ds():
         return
 
     try:
@@ -39,15 +174,20 @@ def load_dataset(directory):
         utils.check_directory(directory)
         utils.balance_dataset(directory)
         utils.split_dataset()
-        print(f"train.py: Loading dataset completed. Data saved at '{DATASET_PATH}'")
+        print("train.py: Loading dataset completed. " +
+              f"Data saved at '{DATASET_PATH}'")
 
     except Exception as e:
         print(f"train.py: Error occurred while loading the dataset: {str(e)}")
 
 
 def main(directory):
-    """Main function to extract and analyze the dataset from images and generate charts."""
+    """
+    Main function to extract and analyze the
+    dataset from images and generate charts.
+    """
     load_dataset(directory)
+    train_model()
 
 
 if __name__ == "__main__":
