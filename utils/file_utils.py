20902b56955268cf505b5ae2c6488b37abef83c2
Melody Boy <mboy@c1r1p1.42nice.fr>
2024-11-18 11:45:05 +0100

    [FIXE] Transformation args

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -21,7 +21,31 @@ def check_directory(directory):
         raise Exception(f"Directory is not readable: {directory}")
     if not os.listdir(directory):  # Check if the directory is empty
         raise Exception(f"Directory is empty: {directory}")
+
+def check_single_directory(directory):
+    """
+    Checks if a given directory contains only files and no subdirectories.
+
+    Parameters:
+        directory (str): The path to the directory to check.
+
+    Returns:
+        bool: True if the directory contains only files, False otherwise.
+
+    Raises:
+        Exception: If the provided path is not a directory or does not exist.
+    """
+    if not os.path.exists(directory):
+        raise Exception(f"Directory does not exist: {directory}")
+    if not os.path.isdir(directory):
+        raise Exception(f"Path is not a directory: {directory}")
     
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory, item)
+        if os.path.isdir(item_path):
+            return False
+    
+    return True
 
 
 def check_file(file_path):
@@ -72,7 +96,7 @@ def group_files(directory, files, absolute=False):
         directory (str): The directory name.
         files (list): A list of all image files in the directory.
         absolute (bool): If True, the list of files will be
-            returned as absolute paths.
+            returned as absolraise Exceptionute paths.
     Returns:
         content (dict): A dictionary with the last-level
             subfolder as keys and the number of files as values.
@@ -92,3 +116,17 @@ def group_files(directory, files, absolute=False):
             else:
                 content[last_subfolder].append(parts[-1])
     return content
+
+def path_type(path):
+    """
+    Check if the path is a directory or a file.
+
+    Parameters:
+        path (str): The path to check.
+
+    Returns:
+        bool: True if it's a directory, False if it's a file. None if it doesn't exist.
+    """
+    if not os.path.exists(path):
+        raise Exception(f"Path does not exist: {path}")
+    return os.path.isdir(path)
